e5966d21050c58b3ae7ef90637b4854e94157ae0
moos97454 <joshirohan2018@outlook.com>
2024-08-12 12:58:45 -0700

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,175 +1,44 @@
 import { link } from "fs";
+import { Space_Mono } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-xl lg:flex">
-        <p className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">rohan.joshi()</code>
-        </p>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center">
-        <p className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">
-            - 👋 Hi, I’m Rohan Joshi <br></br>
-            - 📫 Email: rohanjoshi7678@gmail.com <br></br>
-            - 😄 Pronouns: he/him <br></br>
-            - ⌚ Timezone: UTC - 7:00/PDT <br></br>
-          </code>
-        </p>
-      </div>
-
-      <div className="mb-32 grid text-center lg:m lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
-        <a
-          href="#languages"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Languages{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="#tools"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Tools{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="#projects"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Projects{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-      </div>
-
-      <div id="languages">
-        <h1 className="text-3xl">Languages</h1>
-        <div>
-          <div className="flex">
-            <h1 className="text-lg">Java</h1>
-            <Image
-              src="Logos/Language_logos/Java_logo.svg"
-              alt="Java Logo"
-              width="25"
-              height="60"
-            ></Image>    
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-xl justify-center flex flex-col">
+        <Image
+          className="mb-5"
+          src="Logos/Function_logo.svg"
+          alt="Function Logo"
+          height="100"
+          width="244"
+        ></Image>
+        <p className="max-w-md items-center text-pretty text-xs text-muted-foreground">
+          <a className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline" href="https://www.google.com/maps/place/Dublin,+CA" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-globe h-3 w-3">
+            <circle cx="12" cy="12" r="10"></circle>
+            <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
+            <path d="M2 12h20"></path>
+          </svg>
+          Based in the United States</a></p>
+          <hr></hr>
+          <div>
+          <a href="https://github.com/RohanJoshi2" target="_blank" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-8 w-8">
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" height="20px" width="20px">
+              <path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" data-darkreader-inline-fill="" className="--darkreader-inline-fill: currentColor;"></path>
+            </svg>
+          </a>
+          <a href="mailto:rohanjoshi7678@gmail.com" className="m-1 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-8 w-8">
+            <svg viewBox="0 0 512 512" stroke="currentColor" fill="currentColor" stroke-width="0" height="20px" width="20px" xmlns="http://www.w3.org/2000/svg" data-darkreader-inline-stroke="" className="--darkreader-inline-stroke: currentColor; --darkreader-inline-fill: currentColor;" data-darkreader-inline-fill="">
+              <path d="M437.332 80H74.668C51.199 80 32 99.198 32 122.667v266.666C32 412.802 51.199 432 74.668 432h362.664C460.801 432 480 412.802 480 389.333V122.667C480 99.198 460.801 80 437.332 80zM432 170.667L256 288 80 170.667V128l176 117.333L432 128v42.667z"></path>
+            </svg>
+          </a>
           </div>
-          <div className="flex">
-            <h1 className="text-lg">C++</h1>
-            <Image
-              src="Logos/Language_logos/C++_logo.svg"
-              alt="C++ Logo"
-              width="30"
-              height="25"
-            ></Image>    
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">Python</h1>
-            <Image
-              src="Logos/Language_logos/Python_logo.svg"
-              alt="Python Logo"
-              width="25"
-              height="25"
-            ></Image>   
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">HTML,CSS</h1>
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/HTML_logo.svg"
-              alt="HTML Logo"
-              width="30"
-              height="40"
-            ></Image>   
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/CSS_logos/CSS3_logo.svg"
-              alt="CSS Logo"
-              width="30"
-              height="40"
-            ></Image> 
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">TailwindCSS</h1>
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/CSS_logos/TailwindCSS_logo.svg"
-              alt="TailwindCSS Logo"
-              width="40"
-              height="40"
-            ></Image>
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">Javascript</h1>
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/JS_logos/JavaScript_logo.svg"
-              alt="JavaScript Logo"
-              width="50"
-              height="30"
-            ></Image>
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">ReactJS</h1>
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/JS_logos/ReactJS_logo.svg"
-              alt="React Logo"
-              width="30"
-              height="30"
-            ></Image>
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">NextJS</h1>
-            <div className="bg-white rounded-3xl">
-              <Image
-                src="Logos/Language_logos/Web_Dev_logos/JS_logos/NextJS_logo.svg"
-                alt="Next Logo"
-                width="25"
-                height="30"
-              ></Image>
-            </div>
-          </div>
-          <div className="flex">
-            <h1 className="text-lg">TypeScript</h1>
-            <Image
-              src="Logos/Language_logos/Web_Dev_logos/TypeScript_logo.svg"
-              alt="TypeScript Logo"
-              width="30"
-              height="30"
-            ></Image>
-          </div>
-        </div>
-      </div>
-
-      <div id="tools">
-        <h1 className="text-3xl">Tools</h1>
-        <div className="flex">
-            <h1 className="text-lg">Notion</h1>
-            <Image
-              src="Logos/Software_logos/Notion_logo.svg"
-              alt="Notion Logo"
-              width="30"
-              height="60"
-            ></Image>    
-          </div>
-      </div>
-
-      <div id="projects">
-        <h1 className="text-3xl">Projects</h1>
-        <a></a>
+          <section className="flex min-h-0 flex-col gap-y-3">
+            <h2 className="text-xl font-bold text-[#FFFFFF]">About Me</h2>
+            <p className="text-pretty text-sm text-muted-foreground print:text-[12px]">Currently, I'm a 9th grader @ <span className="hover:underline"><a href="https://dhs.dublinusd.org/" target="_blank" rel="noopener noreferrer">Dublin High School</a></span>
+            .
+            </p></section>
       </div>
     </main>
   );
