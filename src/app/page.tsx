346aa45aa548a3374c27e80d12eb439257e67c8b
moos97454 <joshirohan2018@outlook.com>
2024-08-13 20:14:33 -0700

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,26 @@ export default function Home() {
             </p>
           </section>
       </div>
+      <section className="flex min-h-0 flex-col gap-y-3">
+          <h2 className="text-xl font-bold text-[#FFFFFF]">Work Experience</h2>
+          <div className="rounded-lg bg-card text-card-foreground">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex items-center justify-between gap-x-2 text-base">
+                <h3 className="inline-flex items-center justify-center gap-x-1 font-semibold leading-none">
+                  <a className="hover:underline" href="https://www.thecoderschool.com/">TheCoderSchool</a>
+                  <span className="inline-flex gap-x-1">
+                    <div className="inline-flex items-center rounded-md border px-2 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/60 align-middle text-xs print:px-1 print:py-0.5 print:text-[8px] print:leading-tight">Onsite</div>
+                    <div className="inline-flex items-center rounded-md border px-2 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-nowrap border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/60 align-middle text-xs print:px-1 print:py-0.5 print:text-[8px] print:leading-tight">Volunteer</div>
+                  </span>
+                </h3>
+                <div className="text-sm tabular-nums text-gray-500">2024 July</div>
+              </div>
+              <h4 className="text-sm leading-none print:text-[12px]">Intern
+              </h4>
+            </div>
+            <div className="text-pretty text-muted-foreground mt-2 text-xs print:text-[10px]">As an intern of TheCoderSchool, I</div>
+          </div>
+        </section>
     </main>
   );
 }
