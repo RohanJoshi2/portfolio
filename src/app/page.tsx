a0078a4f58d97155d5bc755ea0954979ab387be4
moos97454 <joshirohan2018@outlook.com>
2024-08-14 19:45:32 -0700

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,8 +43,7 @@ export default function Home() {
             <p className="text-pretty text-sm text-muted-foreground print:text-[12px]">
               Hi, I am a 9th grader at <span className="hover:underline"><a href="https://dhs.dublinusd.org/" target="_blank" rel="noopener noreferrer">Dublin High School</a></span>
               . My coding journey started at a very young age, when I started using scratch in around first grade, which allowed em to easily make my ideas real. Later on in 2017 my parents enrolled me into <span className="hover:underline"><a href="https://www.hackingtons.com/" target="_blank" rel="noopener noreferrer">Hackingtons</a></span>
-              . This is where I started learning web develpment. I use a wide variety of coding languages including: HTML, CSS, JS, Python, Java, C++, Node JS, Prisma ORM, Postgre SQL, React JS, Next JS, and Tailwind CSS.
-              I also use many softwares for coding, like Docker and Figma.
+              . This is where I started learning web develpment. I use a wide variety of coding languages including: HTML, CSS, JS, Python, Java, C++, and many more listed below.
             </p>
           </section>
           <section className="flex min-h-0 flex-col gap-y-3">
