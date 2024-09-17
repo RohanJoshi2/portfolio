b57477db2f444aabff088d69bf05fdd8bbb28cda
moos97454 <joshirohan2018@outlook.com>
2024-09-16 21:37:16 -0700

    Tilt

--- src/components/NestedComponents/Project.tsx
+++ src/components/NestedComponents/Project.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 import { ProjectLanguage } from '@/components/NestedComponents/ProjectLanguage';
 
 interface ProjectProps {
@@ -11,25 +12,34 @@ interface ProjectProps {
 export const Project: React.FC<ProjectProps> = ({ link, title, description, languages }) => {
   return (
     <main>
-      <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border border-muted p-3">
-        <div className="flex flex-col space-y-1.5">
-          <div className="space-y-1">
-            <h3 className="font-semibold tracking-tight text-base">
-              <a href={link} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-1 hover:underline">
-                {title}
-              </a>
-            </h3>
-            <p className="text-muted-foreground text-xs print:text-[10px]">{description}</p>
+      <Tilt
+        className="parallax-effect-glare-scale"
+        perspective={500}
+        glareEnable={true}
+        glareMaxOpacity={0.45}
+        scale={1.02}
+        gyroscope={true}
+      >
+        <div className="rounded-lg bg-card text-card-foreground flex flex-col overflow-hidden border border-muted p-3">
+          <div className="flex flex-col space-y-1.5">
+            <div className="space-y-1">
+              <h3 className="font-semibold tracking-tight text-base">
+                <a href={link} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-1 hover:underline">
+                  {title}
+                </a>
+              </h3>
+              <p className="text-muted-foreground text-xs print:text-[10px]">{description}</p>
+            </div>
+          </div>
+          <div className="text-pretty text-sm text-muted-foreground mt-auto flex">
+            <div className="mt-2 flex flex-wrap gap-1">
+              {languages.map((lang, index) => (
+                <ProjectLanguage key={index} language={lang} />
+              ))}
+            </div>
           </div>
         </div>
-        <div className="text-pretty text-sm text-muted-foreground mt-auto flex">
-          <div className="mt-2 flex flex-wrap gap-1">
-            {languages.map((lang, index) => (
-              <ProjectLanguage key={index} language={lang} />
-            ))}
-          </div>
-        </div>
-      </div>
+      </Tilt>
     </main>
   );
 };
